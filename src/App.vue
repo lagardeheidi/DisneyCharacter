c64824e8459d2417bd69f45fd236e7a537bcc222
lagardeheidi <lagarde.heidi34@gmail.com>
2024-01-15 17:55:48 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <h1>Disney Character</h1>
 <h2><i> The only website to find information about your favorite Disney Character</i></h2>
+<p>Coucou</p>
 </template>
 <script>
 export default {};
