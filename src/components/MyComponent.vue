962263c4b06967ac4a01f66a2f31fa6c6eaee70d
lagardeheidi <lagarde.heidi34@gmail.com>
2024-02-05 16:32:11 +0100

    architecture projet, je réorganise mes dossiers et met des templates de fichiers pour un exemple de component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,21 @@
+// MyComponent.vue
+<template>
+
+    <div id="myComponent">
+        <p>my content</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'myComponent',
+}
+</script>
+
+<style>
+p {
+    text-align: center;
+    font-size: 18px;
+}
+</style>
