f6e6ba2bb670f93fbf4c01245c29b82e90fed440
lagardeheidi <lagarde.heidi34@gmail.com>
2024-03-12 10:18:00 +0100

    enleve commentaire inutiles

--- src/services/api/CharacterRepositery.js
+++ src/services/api/CharacterRepositery.js
@@ -1,20 +1,11 @@
 export default async function getCharacterData(name) {
 	const baseUrl = "http://api.disneyapi.dev/character";
-	// const pageSize = 150; // Nombre de personnages par page
-	// const pagesToFetch = 140; // Nombre de pages à récup
 
 	try {
 		let allCharacters = [];
-		// for (let page = 1; page <= pagesToFetch; page++) {
-		// 	const url = `${baseUrl}?page=${page}&pageSize=${pageSize}`;
-		// 	const response = await fetch(url);
-		// 	const data = await response.json();
-		// 	const characters = data.data;
-		// 	allCharacters = [...allCharacters, ...characters];
-		// }
 
 		// choix arbitraire, pour avoir un truc de base
-		let url = `${baseUrl}?page=1&pageSize=300`;
+		let url = `${baseUrl}?page=1&pageSize=100`;
 
 		if (name) {
 			url = url + "&name=" + name;
